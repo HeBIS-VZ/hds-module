baf45af69a3630d297ddf430cbc9ca0b5b9b2247
Sebastian Böttger <seboettg@gmail.com>
2016-09-20 13:00:14 +0200

    PAIA driver, auth code mechanism and possibility to save access tokens in database

---
+++ src/Hebis/Db/Table/Factory.php
@@ -0,0 +1,54 @@
+<?php
+/*
+ * This file is a part of HDS (HeBIS Discovery System). HDS is an 
+ * extension of the open source library search engine VuFind, that 
+ * allows users to search and browse beyond resources. More 
+ * Information about VuFind you will find on http://www.vufind.org
+ * 
+ * Copyright (C) 2016 
+ * HeBIS Verbundzentrale des HeBIS-Verbundes 
+ * Goethe-Universität Frankfurt / Goethe University of Frankfurt
+ * http://www.hebis.de
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+
+namespace Hebis\Db\Table;
+use Zend\ServiceManager\ServiceManager;
+
+/**
+ * Class Factory
+ *
+ * @author Sebastian Böttger <boettger@hebis.uni-frankfurt.de>
+ */
+class Factory extends \VuFind\Db\Table\Factory
+{
+
+    public static function getUserOAuth(ServiceManager $sm)
+    {
+        $config = $sm->getServiceLocator()->get('VuFind\Config')->get('config');
+        // Use a special row class when we're in privacy mode:
+        $privacy = isset($config->Authentication->privacy)
+            && $config->Authentication->privacy;
+        $rowClass = 'Hebis\Db\Row\UserOAuth';
+        $session = null;
+        if ($privacy) {
+            $sessionManager = $sm->getServiceLocator()->get('VuFind\SessionManager');
+            $session = new \Zend\Session\Container('Account', $sessionManager);
+        }
+        return new UserOAuth($config, $rowClass, $session);
+    }
+}
