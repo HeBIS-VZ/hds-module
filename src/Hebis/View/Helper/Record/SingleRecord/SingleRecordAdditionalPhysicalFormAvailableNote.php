18f7366892c8b328edc3e8be8dccb983c35ef63e
Sebastian Böttger <boettger@hebis.uni-frankfurt.de>
2016-11-22 09:35:02 +0100

    HDS2-21 #time 20m ViewHelper und Test für "Auch als" (SingleRecordOtherEditionEntry). Neue Version der Testtabelle. Nun auch alle Tests für "Andere Ausgaben" erfolgreich.

--- src/Hebis/View/Helper/Record/SingleRecord/SingleRecordAdditionalPhysicalFormAvailableNote.php
+++ src/Hebis/View/Helper/Record/SingleRecord/SingleRecordAdditionalPhysicalFormAvailableNote.php
@@ -26,8 +26,8 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
-namespace Hebis\View\Helper\Record;
-
+namespace Hebis\View\Helper\Record\SingleRecord;
+use Hebis\View\Helper\Record\AbstractRecordViewHelper;
 
 use Hebis\RecordDriver\SolrMarc;
 
