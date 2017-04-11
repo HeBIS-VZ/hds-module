2749ce2b22e100b69c5f92fb88164fb3e99a2525
Sebastian Böttger <boettger@hebis.uni-frankfurt.de>
2017-04-11 11:33:43 +0200

    typo

--- src/Hebis/View/Helper/Hebisbs3/Options.php
+++ src/Hebis/View/Helper/Hebisbs3/Options.php
@@ -39,9 +39,12 @@ class Options extends AbstractHelper
 
     private $options;
 
+    private $edsOptions;
+
     public function __construct(\Zend\ServiceManager\ServiceManager $sm)
     {
         $this->options = $sm->getServiceLocator()->get('VuFind\Config')->get('config');
+        $this->edsOptions = $sm->getServiceLocator()->get('VuFind\Config')->get('eds');
     }
 
     public function theme()
@@ -68,5 +71,4 @@ class Options extends AbstractHelper
     {
         return $this->edsOptions->Facet_Settings->facet_limit;
     }
-
 }
