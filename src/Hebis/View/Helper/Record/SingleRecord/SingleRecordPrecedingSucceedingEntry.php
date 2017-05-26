0ad02ef49c531d704cd8a570ef449da71e44b313
Sebastian Böttger <boettger@hebis.uni-frankfurt.de>
2017-05-26 14:31:11 +0200

    Fehlenden PPN-Suchlink ergänzt

--- src/Hebis/View/Helper/Record/SingleRecord/SingleRecordPrecedingSucceedingEntry.php
+++ src/Hebis/View/Helper/Record/SingleRecord/SingleRecordPrecedingSucceedingEntry.php
@@ -94,25 +94,25 @@ class SingleRecordPrecedingSucceedingEntry extends SingleRecordOtherEditionEntry
         }
 
         if (array_key_exists('a', $subFields) && array_key_exists('t', $subFields) && !empty($w_)) {
-            $link = $this->link($w_[0]->getData());
-            $ret .= '<a href="' . $link . '">';
-            $ret .= htmlentities($subFields['a']) . ": ";
-            $ret .= htmlentities($subFields['t']);
-            $ret .= '</a>';
+
+            $linkText = htmlentities($subFields['a']) . ": ";
+            $linkText .= htmlentities($subFields['t']);
+
+            $ret .= $this->getView()->ppnLink()->getLink($linkText, $this->removePrefix($w_[0]->getData(), "(DE-603)"), ["backlink" => $this->record->getPPN()]);
+
         } else {
             if (array_key_exists('a', $subFields)) {
                 if (!empty($w_)) {
-                    $link = $this->link($w_[0]->getData());
-                    $ret .= '<a href="' . $link . '">' . htmlentities($subFields['a']) . '</a>' . ": ";
+                    $ret .= $this->getView()->ppnLink()->getLink(htmlentities($subFields['a']), $this->removePrefix($w_[0]->getData(), "(DE-603)"), ["backlink" => $this->record->getPPN()]);
                 } else {
-                    $ret .= htmlentities($subFields['a']) . ": ";
+                    $linkText = htmlentities($subFields['a']) . ": ";
+                    $ret .= $this->getView()->ppnLink()->getLink($linkText, $this->removePrefix($w_[0]->getData(), "(DE-603)"), ["backlink" => $this->record->getPPN()]);
                 }
             }
 
             if (array_key_exists('t', $subFields)) {
                 if (!empty($w_)) {
-                    $link = $this->link($w_[0]->getData());
-                    $ret .= '<a href="' . $link . '">' . htmlentities($subFields['t']) . '</a>';
+                    $ret .= $this->getView()->ppnLink()->getLink(htmlentities($subFields['t']), $this->removePrefix($w_[0]->getData(), "(DE-603)"), ["backlink" => $this->record->getPPN()]);
                 } else {
                     $ret .= htmlentities($subFields['t']);
                 }
