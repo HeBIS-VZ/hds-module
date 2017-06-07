a4928e6c49c29ab9514dd9358d4afa5d2f9db2cf
Sebastian Böttger <boettger@hebis.uni-frankfurt.de>
2017-06-07 11:47:29 +0200

    refactoring Test Class

--- tests/unit-tests/src/HebisTest/View/Helper/Record/SingleRecord/SingleRecordPublicationYearTest.php
+++ tests/unit-tests/src/HebisTest/View/Helper/Record/SingleRecord/SingleRecordPublicationYearTest.php
@@ -25,9 +25,9 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
-namespace Hebis\View\Helper\Record\SingleRecord;
+namespace HebisTest\View\Helper\Record\SingleRecord;
 
-use Hebis\View\Helper\Record\AbstractViewHelperTest;
+use HebisTest\View\Helper\Record\AbstractViewHelperTest;
 
 /**
  * Class SingleRecordPublicationDistributionTest
