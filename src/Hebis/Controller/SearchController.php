6618c3ad95d5a104e7c1c3662c01d603c68c52cb
Sebastian Böttger <boettger@hebis.uni-frankfurt.de>
2017-06-26 13:28:12 +0200

    HDS2-142 Katalog-Tab: Anzahl der Treffer hinter der Tab-Überschrift anzeigen

--- src/Hebis/Controller/SearchController.php
+++ src/Hebis/Controller/SearchController.php
@@ -33,6 +33,10 @@ use Hebis\Search\Solr\Results;
 class SearchController extends \VuFind\Controller\SearchController
 {
 
+    const STATUS_OK = 'OK';                  // good
+    const STATUS_ERROR = 'ERROR';            // bad
+    const STATUS_NEED_AUTH = 'NEED_AUTH';    // must login first
+
     public function homeAction()
     {
         $results = $hierarchicalFacets = $hierarchicalFacetSortOptions = [];
@@ -128,4 +132,47 @@ class SearchController extends \VuFind\Controller\SearchController
         // class ID.
         $this->getSearchMemory()->rememberParams($results->getParams());
     }
+
+    /**
+     * @return \Zend\Http\Response
+     */
+    public function ajaxAction()
+    {
+        $this->outputMode = "json";
+        $view = $this->resultsAction();
+        $results = $view->results;
+        $resultTotal = $results->getResultTotal();
+        return $this->output($resultTotal, static::STATUS_OK);
+    }
+
+    /**
+     * Send output data and exit.
+     *
+     * @param mixed $data The response data
+     * @param string $status Status of the request
+     * @param int $httpCode A custom HTTP Status Code
+     *
+     * @return \Zend\Stdlib\ResponseInterface
+     * @throws \Exception
+     */
+    protected function output($data, $status, $httpCode = null)
+    {
+        $response = $this->getResponse();
+        $headers = $response->getHeaders();
+        $headers->addHeaderLine('Cache-Control', 'no-cache, must-revalidate');
+        $headers->addHeaderLine('Expires', 'Mon, 26 Jul 1997 05:00:00 GMT');
+        if ($httpCode !== null) {
+            $response->setStatusCode($httpCode);
+        }
+
+        $headers->addHeaderLine('Content-type', 'application/javascript');
+        $output = ['data' => $data, 'status' => $status];
+        //if ('development' == APPLICATION_ENV && count(self::$php_errors) > 0) {
+        //    $output['php_errors'] = self::$php_errors;
+        //}
+        $response->setContent(json_encode($output));
+        return $response;
+
+    }
+
 }
