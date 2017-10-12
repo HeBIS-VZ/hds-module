5709f003106e04affdc6591f28d05420fefc926b
Sebastian Böttger <boettger@hebis.uni-frankfurt.de>
2017-10-12 22:17:42 +0200

    HDS2-255 Sprache bei Booleschen Operatoren | Unterschiede Treffermenge

--- src/Hebis/Search/Solr/Params.php
+++ src/Hebis/Search/Solr/Params.php
@@ -3,6 +3,10 @@
 namespace Hebis\Search\Solr;
 
 
+use VuFind\Search\QueryAdapter;
+use VuFindSearch\Query\AbstractQuery;
+use VuFindSearch\Query\QueryGroup;
+
 class Params extends \VuFind\Search\Solr\Params
 {
 
@@ -77,5 +81,78 @@ class Params extends \VuFind\Search\Solr\Params
         }
 
         return $filter;
+    }
+
+    /**
+     * Build a string for onscreen display showing the
+     *   query used in the search (not the filters).
+     *
+     * @return string user friendly version of 'query'
+     */
+    public function getDisplayQuery()
+    {
+
+        $showField = [$this->getOptions(), 'getHumanReadableFieldName'];
+
+        // Build display query:
+        return self::display($this->getQuery(), $showField);
+    }
+
+    public static function display(AbstractQuery $query, $showName)
+    {
+        // Simple case -- basic query:
+        if ($query instanceof  \VuFindSearch\Query\Query) {
+            return $query->getString();
+        }
+
+        // Complex case -- advanced query:
+        return self::displayAdvanced($query, $showName);
+    }
+
+    protected static function displayAdvanced(AbstractQuery $query, $showName) {
+        // Groups and exclusions.
+        $groups = $excludes = [];
+
+        foreach ($query->getQueries() as $search) {
+            if ($search instanceof QueryGroup) {
+                $thisGroup = [];
+                // Process each search group
+                foreach ($search->getQueries() as $group) {
+                    if ($group instanceof \VuFindSearch\Query\Query) {
+                        // Build this group individually as a basic search
+                        $thisGroup[]
+                            = call_user_func($showName, $group->getHandler()) . ':'
+                            . $group->getString();
+                    } else {
+                        throw new \Exception('Unexpected ' . get_class($group));
+                    }
+                }
+                // Is this an exclusion (NOT) group or a normal group?
+                $str = join(
+                    ' ' . $search->getOperator()
+                    . ' ', $thisGroup
+                );
+                if ($search->isNegated()) {
+                    $excludes[] = $str;
+                } else {
+                    $groups[] = $str;
+                }
+            } else {
+                throw new \Exception('Unexpected ' . get_class($search));
+            }
+        }
+
+        // Base 'advanced' query
+        $operator = $query->getOperator();
+        $output = '(' . join(') ' . $operator . ' (', $groups) . ')';
+
+        // Concatenate exclusion after that
+        if (count($excludes) > 0) {
+            $output .= ' ' . 'NOT' . ' (('
+                . join(') ' . 'OR' . ' (', $excludes)
+                . '))';
+        }
+
+        return $output;
     }
 }
