a83a8bb654c7a666196842ac0f5ce16e4e34f815
Sebastian Böttger <boettger@hebis.uni-frankfurt.de>
2017-11-29 11:31:48 +0100

    HDS2-295 HDS2-296 HDS2-305 HDS2-306

--- src/Hebis/Search/EDS/Params.php
+++ src/Hebis/Search/EDS/Params.php
@@ -4,6 +4,7 @@ namespace Hebis\Search\EDS;
 
 use VuFind\Search\EDS\QueryAdapter;
 use VuFindSearch\Query\AbstractQuery;
+use VuFindSearch\Query\Query;
 use VuFindSearch\Query\QueryGroup;
 
 class Params extends \VuFind\Search\EDS\Params
@@ -48,48 +49,34 @@ class Params extends \VuFind\Search\EDS\Params
 
     protected static function displayAdvanced(AbstractQuery $query, $showName) {
         // Groups and exclusions.
-        $groups = $excludes = [];
-
+        $output = '';
+        //There should only ever be 1 group with EDS queries.
+        $all = [];
         foreach ($query->getQueries() as $search) {
             if ($search instanceof QueryGroup) {
-                $thisGroup = [];
-                // Process each search group
-                foreach ($search->getQueries() as $group) {
-                    if ($group instanceof \VuFindSearch\Query\Query) {
+                // Process each search group. There should only be 1 with EDS queries
+                $groupQueries = $search->getQueries();
+                for ($i = 0; $i < count($groupQueries); $i++) {
+                    $group = $groupQueries[$i];
+                    if ($group instanceof Query) {
                         // Build this group individually as a basic search
-                        $thisGroup[]
-                            = call_user_func($showName, $group->getHandler()) . ':'
-                            . '<i>' . $group->getString() . '</i>';
+                        $queryOperator = $group->getOperator();
+                        $op = (null != $queryOperator && 0 != $i) ?
+                            $queryOperator . ' ' : '';
+                        $all[] = $op
+                            . call_user_func($showName, $group->getHandler()) . ':'
+                            . $group->getString();
                     } else {
                         throw new \Exception('Unexpected ' . get_class($group));
                     }
-                }
-                // Is this an exclusion (NOT) group or a normal group?
-                $str = join(
-                    ' ' . $search->getOperator()
-                    . ' ', $thisGroup
-                );
-                if ($search->isNegated()) {
-                    $excludes[] = $str;
-                } else {
-                    $groups[] = $str;
                 }
             } else {
                 throw new \Exception('Unexpected ' . get_class($search));
             }
         }
-
-        // Base 'advanced' query
-        $operator = $query->getOperator();
-        $output = '(' . join(') ' . $operator . ' (', $groups) . ')';
-
-        // Concatenate exclusion after that
-        if (count($excludes) > 0) {
-            $output .= ' ' . 'NOT' . ' (('
-                . join(') ' . 'OR' . ' (', $excludes)
-                . '))';
-        }
+        $output = '(' . join(' ', $all) . ')';
 
         return $output;
+
     }
 }
