a142d99460a41e944efdb50e82f47b40869cdd6f
Roshak Zarhoun <roshakz@gmail.com>
2017-12-05 10:01:48 +0100

    Test Data

--- sql/testdata.sql
+++ sql/testdata.sql
@@ -1,11 +1,11 @@
 TRUNCATE static_post;
 INSERT INTO static_post (pid, language, headline, nav_title, content, author, visible) VALUES
-  (11, 'en', 'TestTestTestTestTest-1', 'Test-1', '<p>Hallo <b>Test-1</b></p>', 'T1', 1),
-  (11, 'de', 'ProbeProbeProbe-1', 'Probe-1', '<p>Hallo <b>Probe-1</b></p>', 'T1', 1),
-  (12, 'en', 'TestTestTestTestTest-2', 'Test-2', '<p>Hallo <b>Test-2</b></p>', 'T1', 1),
-  (12, 'de', 'ProbeProbeProbe-2', 'Probe-2', '<p>Hallo <b>Probe-2</b></p>', 'T1', 1),
-  (13, 'en', 'TestTestTestTestTest-3', 'Test-3', '<p>Hallo <b>Test-3</b></p>', 'T1', 1),
-  (13, 'de', 'ProbeProbeProbe-3', 'Probe-3', '<p>Hallo <b>Probe-3</b></p>', 'T1', 1);
+  (11, 'en', 'TestTestTestTestTest-1', 'Test-1', '<p>Hallo <b>Test-1</b></p>', 'admin1', 1),
+  (11, 'de', 'ProbeProbeProbe-1', 'Probe-1', '<p>Hallo <b>Probe-1</b></p>', 'admin1', 1),
+  (12, 'en', 'TestTestTestTestTest-2', 'Test-2', '<p>Hallo <b>Test-2</b></p>', 'admin1', 1),
+  (12, 'de', 'ProbeProbeProbe-2', 'Probe-2', '<p>Hallo <b>Probe-2</b></p>', 'admin1', 1),
+  (13, 'en', 'TestTestTestTestTest-3', 'Test-3', '<p>Hallo <b>Test-3</b></p>', 'admin1', 1),
+  (13, 'de', 'ProbeProbeProbe-3', 'Probe-3', '<p>Hallo <b>Probe-3</b></p>', 'admin1', 1);
 
 
 TRUNCATE broadcasts;
